94883ce3ab3ac1685e9642e9abeb9957d7fd542b
Aaron Beckley <45105699+ambeckley@users.noreply.github.com>
2022-03-28 20:25:37 -0400

    Delete kerrighed-2.4.4 directory

--- kerrighed-2.4.4/linux_version.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-sed '/^vanilla_linux_version/!d; s/.*vanilla_linux_version=//' configure.ac
--- kerrighed-2.4.4/tests/include/krg-tools.h
+++
@@ -1,7 +0,0 @@
-#ifndef KRG_TOOLS_H
-
-#define KRG_TOOLS_H
-
-#include <krgthread.h>
-
-#endif // KRG_TOOLS_H
